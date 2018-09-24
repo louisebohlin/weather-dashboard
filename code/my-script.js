08a0dc39e1876a14c7f8c369a1a6f3a083a25af0
louisebohlin <37780677+louisebohlin@users.noreply.github.com>
2018-09-24 14:46:26 +0200

    Final version with dynamic background colors

--- code/my-script.js
+++ code/my-script.js
@@ -1,24 +1,31 @@
 fetch("http://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=ae7f18bdb699e35f5dd3399dba9247c1")
-.then((response) => {
-  console.log(response)
-  return response.json()
-}).then((data) => {
+  .then((response) => {
+    console.log(response)
+    return response.json()
+  }).then((data) => {
 
-console.log(data)
+    console.log(data)
 
-const city = data.name
-console.log(city)
+    const city = data.name
+    console.log(city)
 
-const temperature = data.main.temp.toFixed(1)
-console.log(temperature)
+    const temperature = data.main.temp.toFixed(1)
+    console.log(temperature)
 
-const weather = data.weather[0].description
-console.log(weather)
+    if (temperature > 25) {
+      document.body.style.backgroundColor = "red"
+    } else if (temperature < 0) {
+      document.body.style.backgroundColor = "lightblue"
+    } else {}
 
-document.getElementById("cityname").innerHTML = city
 
-document.getElementById("temperature").innerHTML = temperature
+    const weather = data.weather[0].description
+    console.log(weather)
 
-document.getElementById("weathertype").innerHTML = weather
+    document.getElementById("cityname").innerHTML = city
 
-})
+    document.getElementById("temperature").innerHTML = temperature + "&degC"
+
+    document.getElementById("weathertype").innerHTML = weather
+
+  })
